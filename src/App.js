d823e9f95975ff170b748f753e4ac1eb9fd98ad0
Fahad <fahhod.ahmad@gmail.com>
2023-05-28 19:17:19 +0300

    Second Task

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import "./App.css";
+import { products } from "./products";
 
 function App() {
+  const productsList = products.map((book) => {
+    return (
+      <div className="data-container">
+        <img src={book.image} />
+        <h5>{book.name}</h5>
+        <p>{book.price}</p>
+      </div>
+    );
+  });
   return (
     <div className="flex-container">
       <div className="card">
         <h2>Coded Bookstore</h2>
         <p>Great Bookstore!</p>
         <img src="https://d1e00ek4ebabms.cloudfront.net/production/77b41724-a1a7-464f-8135-fc7e0f7cec12.jpg" />
+      </div>
+      <div className="products-container">
+        <div className="product-card">{productsList}</div>
       </div>
     </div>
   );
---
+++ src/products.js
@@ -0,0 +1,14 @@
+export const products = [
+  {
+    name: "Bookl1",
+    price: "5.00 KD",
+    image:
+      "https://miblart.com/wp-content/uploads/2020/01/crime-and-mystery-cover-scaled-1.jpeg",
+  },
+  {
+    name: "Bookl2",
+    price: "5.00 KD",
+    image:
+      "https://www.designbookcover.pt/uploads/media/photos/cache/the_pen_and_the_sword_72_media_huge_thumbnail.jpg",
+  },
+];
