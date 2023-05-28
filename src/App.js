120d8d9107e7504896bbecc3171a38b9658401d2
Fahad <fahhod.ahmad@gmail.com>
2023-05-28 17:40:37 +0300

    finished the task

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="flex-container">
+      <div className="card">
+        <h2>Coded Bookstore</h2>
+        <p>Great Bookstore!</p>
+        <img src="https://d1e00ek4ebabms.cloudfront.net/production/77b41724-a1a7-464f-8135-fc7e0f7cec12.jpg" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
