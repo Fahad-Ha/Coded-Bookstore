fde95a1b248e45aac4d96d8a592cbb82cea7766d
Fahad <fahhod.ahmad@gmail.com>
2023-05-29 18:00:56 +0300

    finsied task 3

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import "./App.css";
 import { products } from "./products";
+import Products from "./components/Products";
+import Home from "./components/Home";
 
 function App() {
   const productsList = products.map((book) => {
-    return (
-      <div className="data-container">
-        <img src={book.image} />
-        <h5>{book.name}</h5>
-        <p>{book.price}</p>
-      </div>
-    );
+    return <Products key={book.id} {...book} />;
   });
+
   return (
     <div className="flex-container">
-      <div className="card">
-        <h2>Coded Bookstore</h2>
-        <p>Great Bookstore!</p>
-        <img src="https://d1e00ek4ebabms.cloudfront.net/production/77b41724-a1a7-464f-8135-fc7e0f7cec12.jpg" />
-      </div>
+      <Home />
       <div className="products-container">
         <div className="product-card">{productsList}</div>
       </div>
--- src/products.js
+++ src/products.js
@@ -1,11 +1,13 @@
 export const products = [
   {
+    id: 1,
     name: "Bookl1",
     price: "5.00 KD",
     image:
       "https://miblart.com/wp-content/uploads/2020/01/crime-and-mystery-cover-scaled-1.jpeg",
   },
   {
+    id: 2,
     name: "Bookl2",
     price: "5.00 KD",
     image:
